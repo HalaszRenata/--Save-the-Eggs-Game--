147d31835b581ad92817b3c67a0dfbd173248341
HalaszRenata <146876425+HalaszRenata@users.noreply.github.com>
2024-04-24 17:26:08 +0300

    Add files via upload

---
+++ ==Joc==/==Joc==/==Joc==/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace __Joc__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtMissed = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(78, 17);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score:0";
+            // 
+            // txtMissed
+            // 
+            this.txtMissed.AutoSize = true;
+            this.txtMissed.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMissed.Location = new System.Drawing.Point(12, 46);
+            this.txtMissed.Name = "txtMissed";
+            this.txtMissed.Size = new System.Drawing.Size(88, 17);
+            this.txtMissed.TabIndex = 5;
+            this.txtMissed.Text = "Missed:0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimeEvent);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::@__Joc__.Properties.Resources.chicken_normal;
+            this.player.Location = new System.Drawing.Point(257, 551);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(85, 80);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::@__Joc__.Properties.Resources.egg;
+            this.pictureBox3.Location = new System.Drawing.Point(453, 94);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "egg";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::@__Joc__.Properties.Resources.egg;
+            this.pictureBox2.Location = new System.Drawing.Point(257, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "egg";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::@__Joc__.Properties.Resources.egg;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "egg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = global::@__Joc__.Properties.Resources.f;
+            this.ClientSize = new System.Drawing.Size(620, 631);
+            this.Controls.Add(this.txtMissed);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtMissed;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ ==Joc==/==Joc==/==Joc==/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace __Joc__
+{
+    //In Save The Eggs Game, jucătorul trebuie să salveze cât mai multe ouă.
+    //Un contor reține numărul de ouă salvate, iar un alt contor reține numărul de ouă sparte
+    //Ideea acestui joc este ca jucătorul să prindă obiectele care apar din partea de sus a ecranului
+    //Jucătorul va putea să se deplaseze la stânga și la dreapta,prin butoane, iar
+    //jucătorul va putea, să prindă ouăle care cad sau se sparg
+    //dacă acestea ajung în partea de jos a ecranului sub formă de ouă sparte.
+    //De asemenea, după un număr de obiecte salvate,in cod dupa 10 de elemente, viteza apariției obiectelor
+    //crește, iar picăturile vor deveni mai dese, ceea ce va face jocul mai dificil.
+    public partial class Form1 : Form
+    {
+        bool goleft; //deplasare la stanga
+        bool goright; //deplasare la dreapta
+        int speed = 5;//viteza cu care cad ouale initial
+        int score = 0;//valoarea initiala a scorului
+        int missed = 0;//valoarea initiala pentru ouale pierdute
+        Random rndy = new Random();//valoarea oarecare pentru locatia Y
+        Random rndx = new Random();//valoarea oarecare pentru locatia X
+        PictureBox splash = new PictureBox();//creaza dinamic noi stropi 
+        public Form1()
+        {
+            InitializeComponent();
+            reset();//resetarea jocului
+        }
+    
+
+        private void MainGameTimeEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Eggs Score: " + score; //se afiseaza scorul oualelor salvate
+            txtMissed.Text = "Eggs Missed: " + missed;//se afiseaza scorul oualelor pierdute
+            if (goleft == true && player.Left > 0)
+            {
+                // Daca se acceseaza sageata stanga se deplaseaza gaina cu 12 pixeli
+                player.Left -= 12;
+                //se schimba imaginea gainii cu fata catre stanga
+                player.Image = Properties.Resources.chicken_normal2;
+            }
+            if (goright == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                // Daca se acceseaza sageata dreapta se deplaseaza gaina catre dreapta cu 12 pixeli
+                player.Left += 12;
+                //se schimba imaginea gainii cu fata catre stanga
+                player.Image = Properties.Resources.chicken_normal;
+            }
+            //verificam daca gaina nu a depasit suprafata ferestrei
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox && X.Tag == "egg")
+                {
+                    //daca se coboara un ou si a fost prins creste viteza
+                    X.Top += speed;
+                    //daca oul nu a fost prins apare
+                    // Oul s-a spart, se schimba imaginea
+                    if (X.Top + X.Height > this.ClientSize.Height)
+                    {
+                        splash.Image = Properties.Resources.splash;
+                        //setam locatia oualui care se va sparge
+                        splash.Location = X.Location;
+                        //setează înălțimea imaginii de stropire(splash) la 59 de pixeli. 
+                        splash.Height = 59;
+                        //setam culoarea transparenta a imaginii oualui pierdut
+                        splash.BackColor = System.Drawing.Color.Transparent;
+                        //adaugam imaginea picaturii pe forma
+                        this.Controls.Add(splash);
+                        //pozitionam imaginea picaturii
+                        X.Top = rndy.Next(80, 300);
+                        X.Left = rndx.Next(5, this.ClientSize.Width - X.Width);
+                        //creste contorul oualelor sparte
+                        missed++;
+                        // imaginea vizibila
+                        player.Image = Properties.Resources.chicken_hurt;
+                    }
+                    //daca oul a fost atins de gaina,ambele imagini se intersecteaza
+                    if (X.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        //repozitionam oul aleator
+                        X.Top = rndy.Next(100, 300) * -1;
+                        X.Left = rndx.Next(5, this.ClientSize.Width - X.Width);
+                        //creste scorul oualelor salvate
+                        score++;
+                    }
+                    if (score >= 10)
+                        //daca scorul depaseste 10 puncte creste viteza de coborare a oualelor
+                        speed = 7;
+                    if (score >= 20)
+                        //daca scorul depaseste 20 puncte creste viteza de coborare a oualelor
+                        speed = 8;
+                    if (score >= 30)
+                        //daca scorul depaseste 30 puncte creste viteza de coborare a oualelor
+                        speed = 9;
+                    if (score >= 40)
+                        //daca scorul depaseste 40 puncte creste viteza de coborare a oualelor
+                        speed = 10;
+                    if (score >= 50)
+                        //daca scorul depaseste 50 puncte creste viteza de coborare a oualelor
+                        speed = 11;
+                    //Se castiga doar daca gaina prinde 100 de oua, fara a scapa mai mult de 5 oua
+                    if (score > 100 && missed <= 5)
+                    {
+                        GameTimer.Stop();
+                        MessageBox.Show("Felicitări! Ai câștigat jocul! Ai prins toate cele 100 de ouă fără să pierzi mai mult de 5.");
+                        reset();
+                    }
+                }
+                
+                if (missed > 5)
+                {
+                    GameTimer.Stop();
+                    //se afiseaza mesajul
+                    MessageBox.Show("Joc terminat.Ai pierdut!" + "\r\n" + "Click pe Ok pentru a incepe jocul de la inceput");
+                    //daca se apasa ok se restarteaza jocul
+                    reset();
+                }
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                //daca se apasa tasta sageata stanga jucatorul se deplaseaza spre stanga
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                //daca se apasa tasta dreapta stanga jucatorul se deplaseaza spre dreapta
+                goright = true;
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // În momentul în care tasta stânga sau dreapta este eliberată,
+            // se setează variabila corespunzătoare ca false.
+            if (e.KeyCode == Keys.Left)
+            {
+                // Dacă se eliberează tasta săgeată stânga, se oprește deplasarea către stânga.
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                // Dacă se eliberează tasta săgeată dreapta, se oprește deplasarea către dreapta.
+                goright = false;
+            }
+
+        }
+        private void reset()
+        {
+            //verificam toate elementele jocului
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox && (string)X.Tag == "egg")
+                {
+                    X.Top = rndy.Next(100, 300) * -1;
+                    X.Left = rndx.Next(5, this.ClientSize.Width - X.Width);
+                }
+            }
+            player.Left = this.ClientSize.Width / 2;
+            player.Image = Properties.Resources.chicken_normal2;
+            score = 0;
+            missed = 0;
+            speed = 5;
+            goleft = false;
+            goright = false;
+            GameTimer.Start();
+        }
+    }
+}
